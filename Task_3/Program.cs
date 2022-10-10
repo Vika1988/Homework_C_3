0d15e9f8dd9b7792e902f941d572e53d4d7769ac
*V* <cnevzorov@yandex.ru>
2022-10-10 17:58:30 +0300

    Программа принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N

---
+++ Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число n");
+int n = int.Parse(Console.ReadLine()!);
+int m = 1;
+
+
+if(n < 1)
+{
+    Console.Write("Вы ввели некорректное число n");
+}
+else
+{
+    for (m = 1; m < n; m++)
+    {
+        Console.Write($"{Math.Pow(m, 3)}, ");
+    }
+    Console.WriteLine($"{Math.Pow(m, 3)} ");
+}
